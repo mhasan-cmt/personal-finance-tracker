4587259616548b2ca2b5d4162aa00853988b1542
Mahmudul Hasan Shafin <mahmud.shafin@voicecradle.com>
2024-11-12 10:03:08 +0600

    host changed

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,5 @@
 // const host = "https://expense-tracker-app-knl1.onrender.com";
-const host = "http://localhost:8080";
+const host = 'https://personal-finance-tracker-sepia.vercel.app';
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
